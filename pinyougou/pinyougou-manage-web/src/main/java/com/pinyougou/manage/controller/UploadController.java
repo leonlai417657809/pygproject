32e11b2e587b70ed6de659ca5ae19c307e5be4e8
LeonLai <lzh417657809@163.com>
2019-07-19 10:24:00 +0800

    1.了解网站前台的页面以及广告相关表结构
    2.完成运营商后台广告类型管理与广告管理
    
    3.完成前台工程广告轮播图的展示
    
    4.使用 Spring Data Redis 实现广告数据的缓存

---
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/UploadController.java
@@ -0,0 +1,33 @@
+package com.pinyougou.manage.controller;
+
+import com.pinyougou.common.util.FastDFSClient;
+import com.pinyougou.vo.Result;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequestMapping("/upload")
+@RestController
+public class UploadController {
+    /**
+     * 接收图片并保存图片到fastDFS并返回图片地址
+     * @param file 图片文件
+     * @return 操作结果
+     */
+    @PostMapping
+    public Result upload(MultipartFile file){
+        try {
+            FastDFSClient fastDFSClient = new FastDFSClient("classpath:fastdfs/tracker.conf");
+
+            String file_ext_name = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1);
+
+            String url = fastDFSClient.uploadFile(file.getBytes(),file_ext_name);
+
+            return Result.ok(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+            return Result.fail("上传图片失败");
+    }
+}
